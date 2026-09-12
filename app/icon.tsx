6ee2d857b7e6354998154e9b75f891cbcd8d5cd0
hirakroya50 <hirakroya50@gmail.com>
2026-09-13 02:30:59 +0530

    Update education section to include new entry for Full Stack Web Development from 10x Academy, specifying degree and training year.

---
+++ app/icon.tsx
@@ -0,0 +1,51 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#141210",
+          borderRadius: 9,
+        }}
+      >
+        <div
+          style={{
+            width: 22,
+            height: 22,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: "50%",
+            background: "#ef4444",
+          }}
+        >
+          <span
+            style={{
+              fontSize: 14,
+              fontWeight: 700,
+              color: "#fff7ed",
+              lineHeight: 1,
+              marginTop: 1,
+            }}
+          >
+            h
+          </span>
+        </div>
+      </div>
+    ),
+    size,
+  );
+}
